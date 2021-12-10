b77564940b4d3036e11689a6ddd71d6903fd2cdf
destructiv33 <osmaritur@gmail.com>
2021-12-10 00:56:29 -0600

    404 page

---
+++ pages/404.js
@@ -0,0 +1,28 @@
+import NextLink from 'next/link'
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Heading,
+  Text,
+  Container,
+  Divider,
+  Button
+} from '@chakra-ui/react'
+
+const NotFound = () => {
+  return (
+    <Container>
+      <Heading as="h1">Vaya!</Heading>
+      <Text>Nada por aqui!</Text>
+      <Divider my={6} />
+
+      <Box my={6} align="center">
+        <NextLink href="/">
+          <Button colorScheme="teal" rightIcon={<ChevronRightIcon />}>Regresar al inicio</Button>
+        </NextLink>
+      </Box>
+    </Container>
+  )
+}
+
+export default NotFound
