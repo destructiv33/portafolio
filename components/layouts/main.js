4e892689ce78cad261a0a40958cf3a5f434a5c5c
destructiv33 <osmaritur@gmail.com>
2021-12-11 01:36:57 -0600

    resolve prolems whit 3d model

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
 import NavBar from "../navbar";
 import { Box, Container } from "@chakra-ui/react";
-import VoxelComputer from '../voxel-computer'
+import VoxelComputer from "../voxel-computer";
 
 const Main = ({ children, router }) => {
   return (
-    <Box as="main" pb= { 8 }>
+    <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>destructive - Portafolio</title>
@@ -18,14 +18,14 @@ const Main = ({ children, router }) => {
         <meta property="og:site_name" content="destructive's Homepage" />
         <meta property="og:type" content="website" />
       </Head>
-      <NavBar path={router.asPath}/>
-      <Container maxW="container.md" pt={ 14 }>
-        <VoxelComputer/>
+      <NavBar path={router.asPath} />
+      <Container maxW="container.md" pt={14}>
+        <VoxelComputer />
+
         {children}
       </Container>
     </Box>
   );
-}
+};
 
-
-export default Main
+export default Main;
