ab6553e4749b55cd6e87404403f11877547841c9
destructiv33 <osmaritur@gmail.com>
2021-12-11 00:00:22 -0600

    Tooltip label

--- pages/stack.js
+++ pages/stack.js
@@ -1,13 +1,5 @@
+import { Box, Container, Heading, SimpleGrid, Tooltip } from "@chakra-ui/react";
 import {
-  Box,
-  Container,
-  Heading,
-  List,
-  ListItem,
-  SimpleGrid,
-} from "@chakra-ui/react";
-import {
-  SiCss3,
   SiReact,
   SiAngular,
   SiPython,
@@ -16,17 +8,22 @@ import {
   SiAmazonaws,
   SiMysql,
   SiMicrosoftsqlserver,
-  SiCsharp,
+  SiDotnet,
   SiMicrosoftazure,
   SiPhp,
   SiBootstrap,
   SiTailwindcss,
   SiFlask,
-  SiLaravel
+  SiLaravel,
+  SiVisualstudio,
+  SiVisualstudiocode,
+  SiPowershell,
+  SiNpm,
+  SiComposer,
 } from "react-icons/si";
 
 import Layout from "../components/layouts/article";
-import SkillToLearn from "../components/skill-to-learn";
+import SkillItem from "../components/skill-to-item";
 
 const Stack = () => {
   return (
@@ -38,28 +35,39 @@ const Stack = () => {
           </Heading>
         </Box>
         <SimpleGrid columns={[2, 3, 5, 5]} gap={3}>
-          <SkillToLearn icon={SiAngular} />
-          <SkillToLearn icon={SiReact} />
-          <SkillToLearn icon={SiCsharp} />
-          <SkillToLearn icon={SiMysql} />
-          <SkillToLearn icon={SiMicrosoftsqlserver} /> 
-          <SkillToLearn icon={SiPhp} />
-          <SkillToLearn icon={SiMicrosoftazure} />
-          <SkillToLearn icon={SiBootstrap} />
-          <SkillToLearn icon={SiGithub} />
-          <SkillToLearn icon={SiLaravel} />      
-
+          <SkillItem icon={SiAngular} label={'Angular'}/>
+          <SkillItem icon={SiReact} label={'React'} />
+          <SkillItem icon={SiDotnet} label={'.NET'}/>
+          <SkillItem icon={SiMysql} label={'My SQL'} />
+          <SkillItem icon={SiMicrosoftsqlserver} label={'SQL Server'} />
+          <SkillItem icon={SiPhp} label={'PHP'}/>
+          <SkillItem icon={SiMicrosoftazure} label={'Azure'}/>
+          <SkillItem icon={SiBootstrap} label={'Bootstrap'}/>
+          <SkillItem icon={SiGithub} label={'Git Hub'}/>
+          <SkillItem icon={SiLaravel} label={'Laravek'}/>
         </SimpleGrid>
 
+        <Box>
+          <Heading as="h1" variant="section-title" fontSize={24} mb={2}>
+            Quiero aprender ...
+          </Heading>
+        </Box>
+        <SimpleGrid columns={[2, 3, 5, 5]} gap={3}>
+          <SkillItem icon={SiPython} label={'Python'}/>
+          <SkillItem icon={SiAmazonaws} label={'Amazon AWS'}/>
+          <SkillItem icon={SiTailwindcss} label={'Tailwind CSS'}/>
+          <SkillItem icon={SiFlask} label={'Flask'}/>
+          <SkillItem icon={SiLinux} label={'Linux'}/>
+        </SimpleGrid>
         <Heading as="h1" variant="section-title" fontSize={24} mb={2}>
-          Quiero aprender ...
+          Herramientas ...
         </Heading>
         <SimpleGrid columns={[2, 3, 5, 5]} gap={3}>
-          <SkillToLearn icon={SiPython} />
-          <SkillToLearn icon={SiAmazonaws} />
-          <SkillToLearn icon={SiTailwindcss} />
-          <SkillToLearn icon={SiFlask} />
-          <SkillToLearn icon={SiLinux} />
+          <SkillItem icon={SiVisualstudio} label={'Visual Studio'}/>
+          <SkillItem icon={SiVisualstudiocode} label={'Visual Studio Code'}/>
+          <SkillItem icon={SiNpm} label={'NPM'}/>
+          <SkillItem icon={SiComposer} label={'Compser'}/>
+          <SkillItem icon={SiPowershell} label={'Power Shell'}/>
         </SimpleGrid>
       </Container>
     </Layout>
