1a7ead2f44bb1439553cc53132cd92810c586a59
destructiv33 <osmaritur@gmail.com>
2021-12-10 23:54:50 -0600

    meta data

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -9,6 +9,14 @@ const Main = ({ children, router }) => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>destructive - Portafolio</title>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta name="description" content="Mi portafolio" />
+        <meta name="author" content="Daniel Ramirez" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@destructiv33" />
+        <meta name="twitter:creator" content="@destructiv33" />
+        <meta property="og:site_name" content="destructive's Homepage" />
+        <meta property="og:type" content="website" />
       </Head>
       <NavBar path={router.asPath}/>
       <Container maxW="container.md" pt={ 14 }>
